ef9643863d20e4e441781492b2cf9ec45f98f9ac
MEHEDI <me.mehedi@hotmail.com>
2022-02-02 22:32:15 +0600

    Configure login & register page + Tailwind Config

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chosen.min.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/color-01.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @livewireStyles
 </head>
 <body class="home-page home-01 ">
