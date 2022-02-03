25f9ae7ab2fafe41c724d51d702087f4d03ae70e
MEHEDI <me.mehedi@hotmail.com>
2022-02-04 01:10:10 +0600

    Remove item from cart and clear all from cart

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Cart;
 
 class CartComponent extends Component
 {
+    public function increaseQty($rowId){
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId, $qty);
+    }
+
+    public function decreaseQty($rowId){
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId, $qty);
+    }
+
+    public function destroy($rowId){
+        Cart::remove($rowId);
+        session()->flash('success_message','Item deleted from cart!');
+    }
+
+    public function destroyAll(){
+        Cart::destroy();
+    }
+
+
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
