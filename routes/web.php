067bf48b80386e3c6b94be88da649369b527b964
MEHEDI <me.mehedi@hotmail.com>
2022-02-04 00:50:06 +0600

    Product details and shopping cart setup

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\Admin\AdminDashboardComponent;
 /*
@@ -21,8 +22,9 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 Route::get('/', HomeComponent::class);
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', HomeComponent::class)->name('dashboard');
 Route::get('/shop', ShopComponent::class);
-Route::get('/cart', CartComponent::class);
+Route::get('/cart', CartComponent::class)->name('product.cart');
 Route::get('/checkout', CheckoutComponent::class);
+Route::get('/product/{slug}', DetailsComponent::class)->name('product.details');
 // Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 //     return view('dashboard');
 // })->name('dashboard');
